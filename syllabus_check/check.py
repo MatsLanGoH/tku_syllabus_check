94f95734938ba047444c8fd7cafcc3a4a61207a7
Matthias Lambrecht <niflheim@Matthiass-iMac.local>
2017-09-15 16:05:14 +0900

    Implement modular testing logic

--- syllabus_check/check.py
+++ syllabus_check/check.py
@@ -2,11 +2,23 @@
 
 
 class Check:
+    """
+    A Check object should contain these elements:
+    msg: A string describing the desired result of the check.
+    fun: The test function to conduct the test 
+         (this should return an error message if test fails)
+    args: (Optional) arguments to pass into the test function.
+
+    After instantation, the passes() method is used to determine test result.
+    """
     def __init__(self, msg, fun, *args):
         self.msg = msg
         self.fun = fun
         self.my_args = args
+        self.errors = ""
 
     def passes(self):
-        return self.fun(*self.my_args)
+        # Returns False if an error message has been created.
+        self.errors = self.fun(*self.my_args)
+        return len(self.errors) < 0
 
--- syllabus_check/syllabus_check.py
+++ syllabus_check/syllabus_check.py
@@ -10,10 +10,10 @@ from docx.shared import Inches, RGBColor
 from docx.text.run import Font, Run
 
 # Data positions in excel workbooks
-from positions import *
+from positions import kamoku_info, kamoku_maps
 
 # Check instructions
-from instructions import *
+from instructions import checks
 from check import Check
 
 # Constants
@@ -24,40 +24,67 @@ def check_sheet(sheet):
     """
     Checks a single worksheet.
     """
-    # TODO: Implement this.
-
     # Step 1: Setup environment
-    # (Setup positions and plan grid according to specs)
+    k_items = kamoku_maps[sheet.nrows] 
 
+    result = ""
     # Step 2: Execute checks in checks.
     # NOTE: Make sure we don't have to recreate the check_series each time.
-    pass
+    for check in checks:
+        ck = Check(*check, k_items, sheet)
+        if not ck.passes():
+            result += '\n□ ' + ck.msg
+            result += '\n' + ck.errors
+
+    print(result)
+    return result
 
 
 def execute_checks(workbook):
     """
     Executes checks for a given workbook.
+    Saves result to a docx file containing the name of the source workbook
+    as its file name.
     """
-    print(workbook)
-    
-    # Open workbook
+    # Open workbook and get its name to create output file name
     xl_workbook = open_workbook(workbook)
     result_name = workbook.split('.')[0] + '-tenken.docx'
     result_location = os.path.join('.', RESULTS_FOLDER, result_name)
 
-    # Template file for result output
-    # TODO: NOTE: Make sure this file exists in our path
-    # result_doc = Document(os.path.join(os.environ['RESOURCEPATH'], 'templates/default.docx'))
-    result_doc = Document()
-    result_doc.add_heading('シラバス点検 自動点検結果', 0)
+    # Setup output file
+    document = Document()
+    document.add_heading('シラバス点検 自動点検結果', 0)
 
     # Get the sheets from the workbook
     sheets = xl_workbook.sheets()
 
     # Execute checks for each sheet
     for sheet in sheets:
-        # TODO: Implement this
-        check_sheet(sheet)
+        # Build strings for result output
+        teacher_str = u'\n\t'.join(sheet.cell_value(*kamoku_info['teacher'])
+                                   .split('\n'))
+        class_str = sheet.cell_value(*kamoku_info['name'])
+
+        # Get check results
+        result = check_sheet(sheet)
+
+        # Add results to document
+        document.add_heading('{}: {}'.format(sheet.name, teacher_str), level=2)
+        p = document.add_paragraph()
+        p.add_run('授業科目名: {}'.format(class_str)).bold = True
+        p.paragraph_format.left_indent = Inches(0.5)
+        p.add_run('{}'.format(result))
+
+        # Format colors according to result status
+        if len(result) == 0:
+            p.add_run('\n => 点検OK。')
+        else:
+            p.add_run('\n => 上記の項目を確認し、必要な場合は修正を行ってください。')\
+                    .font.color.rgb = RGBColor(0xFF, 0x00, 0x00)
+        p.add_run('\n' + '_' * 78 + '\n')  # Break line
+
+    # Finally, save result file
+    document.save(result_location)
 
 
 def main():
@@ -81,10 +108,10 @@ def main():
 
     # Check syllabus for all workbooks.
     for wb in workbooks:
-        try:
-            execute_checks(wb)
-        except Exception as e:
-            print("Error:", e)
+        # try:
+        execute_checks(wb)
+        # except Exception as e:
+        #     print("Error:", e)
 
 
 if __name__ == "__main__":
